4d694bed2c68d4902b810cb170a3d1b98d3c83da
DAXILSAXENA <daxildaxil@gmail.com>
2021-05-26 12:49:43 +0530

    Added input of three numbers

---
+++ arithmetic.sh
@@ -0,0 +1,11 @@
+#! /bin/bash -x
+
+echo "Enter any three numbers:"
+
+read -p "Enter first number:" a
+read -p "Enter second number:" b
+read -p "Enter third number:" c
+
+echo "Numbers you entered are:" $a $b $c
+
+
