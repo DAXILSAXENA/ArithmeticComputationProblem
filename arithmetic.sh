865387871fb603d974a820fe98e5ce7e33ddc26a
DAXILSAXENA <daxildaxil@gmail.com>
2021-05-31 15:40:57 +0530

    Computation 4 Added

--- arithmetic.sh
+++ arithmetic.sh
@@ -16,3 +16,6 @@ echo $a*$b+$c = $compute2
 
 compute3=$( echo $a $b $c | awk '{print $3+$1/$2}' )
 echo $c+$a/$b = $compute3
+
+compute4=$( echo $a $b $c | awk '{print $1%$2+$3}' )
+echo $a%$b+$c = $compute4
