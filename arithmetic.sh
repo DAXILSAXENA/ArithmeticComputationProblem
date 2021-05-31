6439d78e74ac4fe2eea8c2f573700d59fc9eca2f
DAXILSAXENA <daxildaxil@gmail.com>
2021-05-31 17:13:04 +0530

    Added Ascending order Sorting

--- arithmetic.sh
+++ arithmetic.sh
@@ -54,3 +54,20 @@ done
 echo "Decending order:"
 echo ${res[*]}
 
+for ((i = 0; i<4; i++))
+do
+
+    for((j = 0; j<4-i-1; j++))
+    do
+
+        if [ ${res[j]} -gt ${res[$((j+1))]} ]
+        then
+            temp=${res[j]}
+            res[$j]=${res[$((j+1))]}
+            res[$((j+1))]=$temp
+        fi
+    done
+done
+
+echo "Ascending order:"
+echo ${res[*]}
